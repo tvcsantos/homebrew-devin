597b28b81305bada24718784b7b45f3ad42c9817
tvcsantos <112688+tvcsantos@users.noreply.github.com>
2026-05-08 09:13:34 +0000

    [create-pull-request] automated change

--- Formula/devin.rb
+++ Formula/devin.rb
@@ -4,17 +4,17 @@
 class Devin < Formula
   desc "Command-line coding agent powered by Devin's intelligence"
   homepage "https://cli.devin.ai"
-  version "2026.4.24-1"
+  version "2026.5.6-1"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://static.devin.ai/cli/#{version}/devin-#{version}-aarch64-apple-darwin.tar.gz"
-      sha256 "818a5dabc9f64bb296968fe79ade8c4ea289a143037ec124c9e18410f5e7e2b5"
+      sha256 "ad565a88bd225f260699e2fd0330185c5488ada68043a0d937487fd3c98eab2c"
     end
     on_intel do
       url "https://static.devin.ai/cli/#{version}/devin-#{version}-x86_64-apple-darwin.tar.gz"
-      sha256 "bd1f1b20e981bef3dae761b3af467f90c71fa421d033cf161bd41a3678dc6fda"
+      sha256 "32679444e048099e54b6769e2246534bd5cff46a3be478f26189221d8b450e8d"
     end
   end
 
